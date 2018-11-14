603f710ec006e5a73033857eaeff241e3f60af6d
Lling <ling.li05@hand-china.com>
2018-11-14 20:47:40 +0800

    [MOD]add annotations；

---
+++ src/main/java/com/mmall/concurrency/annotations/UnThreadSafe.java
@@ -0,0 +1,15 @@
+package com.mmall.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程不安全的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface UnThreadSafe {
+    String value() default "";
+}
